2f24a52fd50baef7bef096778cc4886de1491425
ahamideh <ahamideh18@hotmail.com>
2023-01-25 18:42:31 -0500

    Add upload routes

---
+++ server/middleware/isAuthenticated.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export default function (req: Request, res: Response, next: NextFunction) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).send("Unauthorized");
+  }
+  try {
+    jwt.verify(token, process.env.JWT_SECRET as string);
+    next();
+  } catch (err) {
+    return res.status(401).send("Unauthorized");
+  }
+}
---
+++ server/routers/upload.ts
@@ -0,0 +1,39 @@
+import express, { Request } from "express";
+import multer, { Options } from "multer";
+
+import isAuthenticated from "../middleware/isAuthenticated";
+
+type Limits = Options["limits"];
+
+const router = express.Router();
+
+const secret = process.env.JWT_SECRET as string;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + "_" + Date.now());
+  },
+});
+const limits: Limits = { fileSize: 10 * 1024 * 1024, files: 20 };
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({ storage, limits, fileFilter });
+
+router.post("/", isAuthenticated, upload.any(), async (req, res) => {
+  res.send("Files uploaded");
+});
+
+export default router;
