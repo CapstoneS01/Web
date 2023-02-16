08b33e2830d9eac857de5d2ee2af8b54a9167f2d
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-02-16 17:53:42 -0500

    removed firebase and updated login screen

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,24 +1,37 @@
 import React, { useEffect } from "react";
 import logo from "../assests/skyhunt-logo.png";
-import { OAuthLogin } from "../auth/OAuthLogin";
-import { auth } from "../auth/firebase";
 import { Navigate } from "react-router-dom";
 
 function Login() {
-  const [user, setUser] = React.useState(auth.currentUser);
+  // const [user, setUser] = React.useState(auth.currentUser);
+
+  function sendLink() {}
 
   return (
-    <div className="flex h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
-      <div className="bg-white m-auto rounded-lg h-full w-full lg:w-4/12 lg:h-2/3 ">
+    <div className="flex h-screen bg-gradient-to-r from-cyan-400 to-blue-500">
+      <div className="bg-white m-auto rounded-lg h-full w-full lg:w-4/12 lg:h-2/5 ">
         <div className="flex text-left px-3 py-5">
           <img src={logo} alt="SkyHunt" className="h-10 m-auto" />
         </div>
-        <div className="header-text px-10 py-7">
+        <div className="header-text px-10 py-4 text-center">
           <h1 className="text-2xl font-bold text-gray-700">Get Started</h1>
           <h3 className="text-base text-gray-500 pt-1">Continue to SkyHunt</h3>
         </div>
-        <div className="login-methods flex mt-20">
-          <OAuthLogin />
+        <div className="flex md:w-4/5 mx-auto my-3">
+          <input
+            className="px-3 py-3 placeholder-slate-400 text-slate-700 relative bg-white rounded text-sm border border-slate-100 shadow focus:outline-none focus:ring w-full"
+            placeholder="Email"
+            type="email"
+          />
+        </div>
+        <div className="flex md:w-full my-3">
+          <button
+            onClick={sendLink}
+            type="button"
+            className=" w-2/3 mx-auto mt-7 text-white font-bold bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            <span className="font-semibold">Send Link</span>
+          </button>
         </div>
       </div>
     </div>
