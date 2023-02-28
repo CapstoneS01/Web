10b6ec566ffacd77d75e5e6fe8a5e3fb352536d2
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-02-28 11:53:20 -0500

    Working on welcome page and sidebar

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
+import { Sidebar } from "../components/Sidebar";
 
 const Dashboard = () => {
-  // const [user, setUser] = React.useState(auth.currentUser);
+  // submit image function
+  //      <input type="file" onChange={(e) => submitImage(e!.target!.files![0])} />
+  async function submitImage(file: any) {
+    const data = new FormData();
+    data.append("file", file);
+
+    await fetch("http://localhost:3030/upload", {
+      credentials: "same-origin",
+      method: "POST",
+      body: data,
+      headers: {
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjAxR1FTWkMwMFJYSDdXTUJUMlhIWENTMEdaIiwiaWF0IjoxNjc0ODM1MjYzfQ.yneN_EBmUNPFK9zsPuXQHlZTn-FWRkERXBC0hd6-Bi8",
+      },
+    });
+  }
 
   return (
-    // if no authenticated user, navigate to login page using react-router-dom
-    <div>
-      {/* {!user && <Navigate to="/login" />} */}
-      <p>Welcome to the dashboard</p>
-      <a onClick={() => null}>Logout</a>
+    <div className="h-screen text-center justify-center">
+      <Sidebar />
     </div>
   );
 };
--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -1,25 +1,36 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import logo from "../assests/skyhunt-logo.png";
 
 function Welcome() {
-  async function submitImage(file: any) {
-    const data = new FormData();
-    data.append("file", file);
-
-    await fetch("http://localhost:3030/upload", {
-      credentials: "same-origin",
-      method: "POST",
-      body: data,
-      headers: {
-        token:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjAxR1FTWkMwMFJYSDdXTUJUMlhIWENTMEdaIiwiaWF0IjoxNjc0ODM1MjYzfQ.yneN_EBmUNPFK9zsPuXQHlZTn-FWRkERXBC0hd6-Bi8",
-      },
-    });
-  }
-
   return (
-    <div>
-      <h1 className="">Welcome Page</h1>
-      <input type="file" onChange={(e) => submitImage(e!.target!.files![0])} />
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <header className="absolute top-0 right-0 flex items-center justify-between w-full px-4 py-3 content-center">
+        <img src={logo} width={250} alt="SkyHunt Logo" className="my-auto" />
+        <a
+          className="text-gray-100 hover:text-gray-300 mb-2"
+          href="https://github.com/CapstoneS01"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Star on GitHub"
+        >
+          <FaGithub size={28} />
+        </a>
+      </header>
+      <div className="flex flex-col items-center justify-center py-20 bg-white rounded-lg px-5">
+        <h2 className="text-lg font-medium text-gray-900 text-center">
+          Enabling drones with computer vision and object detection, optimized
+          for missing person searches
+        </h2>
+        <Link
+          to="/login"
+          className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:shadow-outline-blue"
+        >
+          Get Started
+        </Link>
+      </div>
     </div>
   );
 }
